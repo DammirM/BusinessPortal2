dec37937978efe34ee005dae32da7c34509878d2
Damir Mehic <damir.mehic@edu.varberg.se>
2023-10-16 12:40:33 +0200

    Created Leave Controller

---
+++ BusinessPortal2/Data/PersonaldataContext.cs
@@ -0,0 +1,19 @@
+﻿using BusinessPortal2.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BusinessPortal2.Data
+{
+    public class PersonaldataContext : DbContext
+    {
+
+        public PersonaldataContext(DbContextOptions<PersonaldataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Personal> personals {get; set;}
+        public DbSet<LeaveType> leaveTypes {get; set;}
+
+
+    }
+}
--- BusinessPortal2/Services/iLeaveTypeRepository.cs
+++ BusinessPortal2/Services/iLeaveTypeRepository.cs
@@ -1,8 +1,16 @@
-﻿namespace BusinessPortal2.Services
-{
-    public class iLeaveTypeRepository
-    {
+﻿using BusinessPortal2.Models;
 
-        
+namespace BusinessPortal2.Services
+{
+    public interface ILeaveTypeRepository
+    {
+        Task<IEnumerable<LeaveType>> GetAll();
+
+        Task<LeaveType> GetById(int id);
+
+        Task<LeaveType> UpdateLeave(int id, LeaveType tr);
+
+        Task<LeaveType> DeleteLeave(int personalId);
+
     }
 }
