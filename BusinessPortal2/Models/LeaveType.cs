65c53b3d07bc01d04352bdd8d8147016a62b894c
Damir Mehic <damir.mehic@edu.varberg.se>
2023-10-16 11:54:04 +0200

    added leave props

--- BusinessPortal2/Models/LeaveType.cs
+++ BusinessPortal2/Models/LeaveType.cs
@@ -1,6 +1,20 @@
-﻿namespace BusinessPortal2.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BusinessPortal2.Models
 {
     public class LeaveType
     {
+
+        [Key]
+        [ForeignKey("Personal")]
+        public int PersonalId { get; set; }
+        public int Vabb { get; set; }
+        public int Sick { get; set; }
+        public int Vacation { get; set; }
+
+        // public Personal Personal { get; set; } // Add this navigation property
+
+
     }
 }
