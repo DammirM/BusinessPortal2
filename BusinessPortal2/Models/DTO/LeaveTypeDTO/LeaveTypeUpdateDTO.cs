05536565006602b36bdcfa3dc7c19b487bf3e79c
MaxDBerg <max.dahlberg@gmail.com>
2023-10-18 13:48:30 +0200

    Added DTO's for LeaveType model

---
+++ BusinessPortal2/Models/DTO/LeaveTypeDTO/LeaveTypeUpdateDTO.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BusinessPortal2.Models.DTO.LeaveTypeDTO
+{
+    public class LeaveTypeUpdateDTO
+    {
+        public int Id { get; set; }
+        [Required]
+        public string LeaveName { get; set; }
+        [Required]
+        public int LeaveDays { get; set; }
+        [Required]
+        public int PersonalId { get; set; }
+    }
+}
