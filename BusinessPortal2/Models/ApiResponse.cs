1be59a17ac5fd8a8ba6f8bef093017bd8bab695c
Damir Mehic <damir.mehic@edu.varberg.se>
2023-10-23 10:08:42 +0200

    Changed to .NET 7

--- BusinessPortal2/Models/ApiResponse.cs
+++ BusinessPortal2/Models/ApiResponse.cs
@@ -9,8 +9,7 @@ namespace BusinessPortal2.Models
             Errors = new List<string>();
         }
 
-        public Object Result { get; set; 
-        public object body { get; set; }
+        public Object Result { get; set; }
         public HttpStatusCode StatusCode { get; set; }
         public bool isSuccess { get; set; }
         public List<string> Errors { get; set; }
